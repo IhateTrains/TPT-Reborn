db2ab83e06f75c266ee250c46eac04865a057f8c
IlikeTrains <sewerynpresnal@gmail.com>
2018-12-28 16:13:12 +0100

    Using TPT 2.1.2 as a base

---
+++ The Prince and the Thane Reborn.mod
@@ -0,0 +1,3 @@
+name = "The Prince and the Thane Reborn"
+path = "mod/The Prince and the Thane"
+user_dir = "The Prince and the Thane"
---
+++ create_symbolic_links.bat
@@ -0,0 +1,2 @@
+mklink /J "%HOMEDRIVE%%HOMEPATH%\Documents\Paradox Interactive\Crusader Kings II\mod\The Prince and the Thane" ".\The Prince and the Thane"
+pause
